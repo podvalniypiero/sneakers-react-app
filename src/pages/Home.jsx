7d067572838a6535a4f16f90b64556e8dc565afc
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-03-11 15:11:09 +0300

    fix searching in search-block

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,14 +31,13 @@ function Home({
         <h1>{searchValue.toString().length === 0 ? `Все кроссовки`: `Поиск по запросу: "${searchValue}"`}</h1>
         <div className="search-block d-flex">
           <img src="img/search.svg" alt="Search" />
-          {searchValue && (
-            <img
+          {searchValue.toString().length !== 0 && <img
               onClick={() => setSearchValue('')}
               className="clear cu-p"
               src="img/btn-remove.svg"
               alt="Clear"
-            />
-          )}
+            />  
+          }
           <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
         </div>
       </div>
